53b829444884ffd43e715a2e992e01c1923ab9bf
tsingbin <tantsingbin@sina.com>
2025-09-28 09:51:19 +0800

    fix: 更新了一点配置

--- vite.config.ts
+++ vite.config.ts
@@ -145,7 +145,7 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
       proxy: {
         // 选项写法
         '/api': {
-          target: 'http://127.0.0.1:5002',
+          target: 'http://127.0.0.1:5050',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, '')
         }
